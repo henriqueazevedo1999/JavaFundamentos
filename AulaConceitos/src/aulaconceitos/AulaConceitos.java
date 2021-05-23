301c6cf7eac3d822c7952913efaa4034d4f1d9bc
Henrique <70726@RED10.proway.treina>
2021-05-23 18:30:13 -0300

    adicionado método Verifica Idade

--- AulaConceitos/src/aulaconceitos/AulaConceitos.java
+++ AulaConceitos/src/aulaconceitos/AulaConceitos.java
@@ -16,16 +16,30 @@ import java.text.ParseException;
  */
 public class AulaConceitos
 {
+
     /**
      * @param args the command line arguments
      */
-    
     public static void main(String[] args)
     {
-        String name = lerDadosTeclado("Informe seu nome: ");
-        int idade = Integer.parseInt(lerDadosTeclado("Informe sua idade: "));
-        int altura = Integer.parseInt(lerDadosTeclado("Informe sua altura em cm: "));
-        double peso = Double.parseDouble(lerDadosTeclado("Informe seu peso em kg: "));
+//        int altura = Integer.parseInt(lerDadosTeclado("Informe sua altura em cm: "));
+//        double peso = Double.parseDouble(lerDadosTeclado("Informe seu peso em kg: "));
+//        System.out.print("Insira x: ");
+//        double x = Double.parseDouble(tela.nextLine());
+//        System.out.print("Insira y: ");
+//        double y = Double.parseDouble(tela.nextLine());
+//        System.out.print("Insira z: ");
+//        double z = Double.parseDouble(tela.nextLine());
+//
+//        operadoresMatematicos(x, y);
+//        operadoresComparacao(x, y);
+//        operadoresLogicos(x, y, z);
+
+        String name = lerDadosTeclado("Informe o nome: ");;
+        int age = Integer.parseInt(lerDadosTeclado("Informe a idade: "));
+        System.out.println();
+        System.out.println("Nome: " + name);
+        System.out.println("Situação: " + verificaIdade(age));
     }
 
     public static String lerDadosTeclado(String message)
@@ -43,9 +57,63 @@ public class AulaConceitos
         double peso = 10.5;
         float altura = (float) 1.69;
     }
-    
-    public static void operadores()
+
+    public static void operadoresMatematicos(double x, double y)
     {
-        
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x / y = " + (x / y));
+        System.out.println("x % y = " + (x % y));
+    }
+
+    public static void operadoresComparacao(double x, double y)
+    {
+        System.out.println("x == y? " + (x == y));
+        System.out.println("x != y? " + (x != y));
+        System.out.println("x >= y? " + (x >= y));
+        System.out.println("x > y? " + (x > y));
+        System.out.println("x <= y? " + (x <= y));
+        System.out.println("x < y? " + (x < y));
+    }
+
+    public static void operadoresLogicos(double x, double y, double z)
+    {
+        System.out.println("x < y and x < z? " + ((x < y) && (x < z)));
+        System.out.println("x < y and x > z? " + ((x < y) && (x > z)));
+        System.out.println("x > y and x < z? " + ((x > y) && (x < z)));
+        System.out.println("x > y and x > z? " + ((x > y) && (x > z)));
+    }
+
+    public static void desvioCondicional(double x, double y)
+    {
+        if (x < y)
+        {
+            System.out.println("x < y!");
+        } else if (x > y)
+        {
+            System.out.println("x > y!");
+        } else if (x == y)
+        {
+            System.out.println("x = y!");
+        }
+    }
+
+    public static String verificaIdade(int idade)
+    {
+        if (idade <= 10)
+        {
+            return TipoIdade.Crianca.toString();
+        } else if (idade < 15)
+        {
+            return TipoIdade.Pre_Adolescente.toString();
+        } else if (idade < 18)
+        {
+            return TipoIdade.Adolescente.toString();
+        } else if (idade < 50)
+        {
+            return TipoIdade.Adulto.toString();
+        }
+        return TipoIdade.Terceira_Idade.toString();
     }
 }
