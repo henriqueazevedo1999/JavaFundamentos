cb67d89b5341b59a49933bbd4e1742ae859bd24c
70726 <70726@RED10.proway.treina>
2021-06-20 15:41:49 -0300

    Corrigido arquivos com erro

--- AulaConceitos/src/aulaconceitos/AulaConceitos.java
+++ AulaConceitos/src/aulaconceitos/AulaConceitos.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aulaconceitos;
 
 import java.util.Locale;
@@ -10,10 +5,6 @@ import java.util.Scanner;
 import java.text.NumberFormat;
 import java.text.ParseException;
 
-/**
- *
- * @author 70726
- */
 public class AulaConceitos
 {
 
